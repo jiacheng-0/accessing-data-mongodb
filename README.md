cd7879309318ed237cee6bcee6c8e71ad0814f0d
Jia Cheng <michael-tjc@hotmail.com>
2021-05-26 01:17:58 +0800

    complete

---
+++ README.md
@@ -0,0 +1,16 @@
+# accessing-data-mongodb
+
+https://spring.io/guides/gs/accessing-data-mongodb/
+
+## Overall diagram
+
+![java](C:\VisaMaven\accessing-data-mongodb\diagrams\java.png)
+
+## Running the project
+
+```shell
+mvnw spring-boot:run
+```
+
+
+
--- src/main/java/com/example/accessingdatamongodb/AccessingDataMongodbApplication.java
+++ src/main/java/com/example/accessingdatamongodb/AccessingDataMongodbApplication.java
@@ -1,13 +1,55 @@
 package com.example.accessingdatamongodb;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccessingDataMongodbApplication {
+public class AccessingDataMongodbApplication implements CommandLineRunner {
+
+	@Autowired
+	private CustomerRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccessingDataMongodbApplication.class, args);
+	}
+
+	/**
+	 * 
+	 *
+	 * @param args
+	 * @throws Exception
+	 */
+	@Override
+	public void run(String... args) throws Exception {
+
+		repository.deleteAll();
+
+		// save a couple of customers
+		repository.save(new Customer("Alice", "Smith"));
+		repository.save(new Customer("Bob", "Smith"));
+
+		// fetch all customers
+		System.out.println("\nCustomers found with findAll():");
+		System.out.println("-------------------------------");
+		for (Customer customer : repository.findAll()) {
+			System.out.println(customer);
+		}
+		System.out.println();
+
+		// fetch an individual customer
+		System.out.println("\nCustomer found with findByFirstName('Alice'):");
+		System.out.println("--------------------------------");
+		System.out.println(repository.findByFirstName("Alice"));
+
+		System.out.println("\nCustomers found with findByLastName('Smith'):");
+		System.out.println("--------------------------------");
+		for (Customer customer : repository.findByLastName("Smith")) {
+			System.out.println(customer);
+		}
+		System.out.println("");
+
 	}
 
 }
